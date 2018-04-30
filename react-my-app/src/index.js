1a193adf0576e18c52ed61eff05740d90c8c7358
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-04-30 23:21:10 +0200

    update

--- react-my-app/src/index.js
+++ react-my-app/src/index.js
@@ -8,3 +8,7 @@ import Calculator from './components/Calculator'
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Calculator operation={'+'} />, document.getElementById('root'));
 registerServiceWorker();
+
+const routes = (
+    <h1>Hello World</h1>
+  );
