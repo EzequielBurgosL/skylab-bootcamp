f3cf992650398c8ededcc693f358a9ea4e93d465
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-19 08:57:27 +0200

    update

--- learning_tutorials/udemy-nodejs/app.js
+++ learning_tutorials/udemy-nodejs/app.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+console.log('starting app.js')
+
+const fs = require('fs')
+const _ = require('lodash')
+
+const notes = require('./notes.js')
+
+var command = process.argv[2]
+console.log('Command: ', command)
+console.log(process.argv)
+
+if (command === 'add'){
+    console.log('adding new note')
+}else if(command === 'list') {
+    console.log('listing all notes')
+}else if(command === 'read') {
+    console.log('reading note')
+}else if(command === 'remove') {
+    console.log('removing note')
+}else{
+    console.log('command not found')
+}
