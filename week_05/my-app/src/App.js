5cd870edaebf6fe192cfd955fd802aa6cc4e9c74
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-21 18:54:29 +0200

    update

--- week_05/my-app/src/App.js
+++ week_05/my-app/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+
+const App = function () {
+  // change code below this line
+  
+  const JSX = (
+    <h2>hello world</h2>   
+  )
+  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div>
+        <h1>{JSX}</h1>
       </div>
-    );
+    )
+
+
   }
-}
+
+
+
+  // change code above this line
+
+
 
 export default App;
---
+++ week_06/nodejs/index.js
@@ -0,0 +1,5 @@
+const[_node, _process, oper1, oper2] = process.argv
+
+const num1 = parseInt(oper1), num2 = parseInt(oper2)
+
+console.log(num1 + num2)
